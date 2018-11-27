381d1f4908b96662721b0f9cb726b654f58e813e
Joshua Bearden <josh.e.bearden@gmail.com>
2018-11-27 17:34:04 -0600

    undoing bugfix?

--- parsenode.h
+++ parsenode.h
@@ -5,15 +5,15 @@
 #ifndef PARSENODE_H
 #define PARSENODE_H
 
-typedef struct
+typedef struct parseNode_t
 {
     char * nonTerm;
     char ident[8];
     char op[3];
     char integr[8];
-    parseNode * leftSub;
-    parseNode * midSub;
-    parseNode * rightSub;
+    parseNode_t * leftSub;
+    parseNode_t * midSub;
+    parseNode_t * rightSub;
 } parseNode;
 
 #endif //PARSENODE_H
